4822903af280cbe7a0c9ec690a07ffb7b36b8d3d
Mathieu Kohl <mathieu.kohl@m4finvest.com>
2022-12-13 14:58:09 +0100

    small details

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 
-
-
-
-
 function App() {
   return (
     <div className="App">
