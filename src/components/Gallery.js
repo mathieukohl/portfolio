91030541fc0b20cdd9350a243d3c13bb745e4964
Mathieu <mathieukohl@hotmail.com>
2024-01-29 13:52:35 +0100

    fixing scrolling problem

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -10,36 +10,44 @@ export const Gallery = () => {
     <div className="gallery_body">
         <div className="gallery">
             <figure className="card">
-            <a href="https://www.stayinmtn.com/" target="_blank" rel="noreferrer">
-                <img
-                    src={projImg1}
-                    alt="img1"
-                />
+                <a href="https://www.stayinmtn.com/" target="_blank" rel="noreferrer">
+                    <img
+                        src={projImg1}
+                        alt="img1"
+                    />
                 </a>
             </figure>
             <figure className="card">
-                <img
-                    src={projImg2}
-                    alt="img1"
-                />
+                <a href="https://www.melyjones.ch/" target="_blank" rel="noreferrer">
+                    <img
+                        src={projImg2}
+                        alt="img1"
+                    />
+                </a>
             </figure>
             <figure className="card">
-                <img
-                    src={projImg3}
-                    alt="img1"
-                />
+                <a href="https://www.ck-ecom.com/" target="_blank" rel="noreferrer">
+                    <img
+                        src={projImg3}
+                        alt="img1"
+                    />
+                </a>
             </figure>
             <figure className="card">
-                <img
-                    src={automarket}
-                    alt="img1"
-                />
+                <a href="https://automarket.ch/" target="_blank" rel="noreferrer">
+                    <img
+                        src={automarket}
+                        alt="img1"
+                    />
+                </a>
             </figure>
             <figure className="card">
-                <img
-                    src={carsloc}
-                    alt="img1"
-                />
+                <a href="https://carsloc.ch/" target="_blank" rel="noreferrer">
+                    <img
+                        src={carsloc}
+                        alt="img1"
+                    />
+                </a>
             </figure>
         </div>
     </div>
